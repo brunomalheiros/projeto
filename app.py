c379c4e633c852f6fc77d56f02b1ca06d5f67369
Bruno Malheiros <brunopma10@gmail.com>
2019-11-15 15:42:32 -0300

    Configuraçã do banco de dados da aplicação,modelando usuario

--- app.py
+++ app.py
@@ -1,6 +1,28 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///banco.db'
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __init__(self, username, email, senha):
+        self.username = username
+        self.email = email
+        self.senha = generate_password_hash(senha)
+
+
+
+
+db.create_all()
 
 
 @app.route('/')
