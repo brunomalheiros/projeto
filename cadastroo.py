1efecf74b14ba26958e7d6cd2e79d7ec54681b68
Bruno Malheiros <brunopma10@gmail.com>
2019-12-04 23:07:40 -0300

    pequena alteração nesse arquivo

--- cadastroo.py
+++ cadastroo.py
@@ -3,7 +3,7 @@ from wtforms import StringField, SubmitField, PasswordField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 class CadastroForm(FlaskForm):
-    username = StringField('Nome de usuário',validators=[DataRequired(), Length(min=2, max=50)])
+    username = StringField('Nome Usuário',validators=[DataRequired(), Length(min=2, max=50)])
     email = StringField('Email',validators=[DataRequired(), Email()])
     senha = PasswordField('Senha',validators=[DataRequired(), Length(min=4, max=20)])
     cadastrar = SubmitField('Cadastrar')
