ca099b5987fd115dfabfd5444bfbb0b397fddcd0
Bruno Malheiros <brunopma10@gmail.com>
2019-12-04 23:07:54 -0300

    pequena alteração nesse arquivo

--- login.py
+++ login.py
@@ -4,5 +4,5 @@ from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
-    senha = PasswordField('Senha', validators=[DataRequired(), Length(min=4, max=80)])
+    senha = PasswordField('Senha', validators=[DataRequired()])
     entrar = SubmitField('Entrar')
